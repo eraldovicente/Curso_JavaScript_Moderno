092b83f5188a4076af4eba29ddab3e04ea0f892e
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-04 20:56:17 -0300

    Introduccion a las variables y comentarios

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,3 +1,13 @@
 
 
-console.log( console.log('Hola Mundo') );
+// console.log( console.log('Hola Mundo') );
+
+let  a = 10,
+     b = 20,
+     c = 10,
+     d = 10,
+     x = a + b;
+
+console.log( x );
+
+// c = 20; // no se puede hacer
