e5c79f705bb5edfc1a02d96868eb2179e7de9d78
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-04 21:18:58 -0300

    Introduccion a la consola

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -4,10 +4,17 @@
 
 let  a = 10,
      b = 20,
-     c = 10,
-     d = 10,
+     c = 'Hola ',
+     d = 'Spiderman',
      x = a + b;
 
-console.log( x );
+     const saludo = c + d;
+
+     // console.table({a, b, c, d, x});
+
+     // console.log('%c Mis variables', 'color:blue; font-weight: bold');
+     // console.log({ b });
+     // console.log({ c });
+     // console.log({ d });
 
 // c = 20; // no se puede hacer
