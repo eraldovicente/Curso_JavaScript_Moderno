08f8ed618b864f60004af6cf930bc9bd190d24d2
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-05 14:42:58 -0300

    Palavras reservadas y nombre de variables

---
+++ 01-fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,30 @@
+
+// let const = 123;
+
+let await = 123;
+
+// const
+
+// if
+
+// for()
+
+var ლ_ಠ益ಠ_ლ = 42;
+
+console.log( ლ_ಠ益ಠ_ლ );
+
+var 〱〱 = 2;
+
+console.log(〱〱);
+
+console.log(〱〱 << 〱〱); 
+
+
+// let 1objeto = 123;
+// let objeto123.123 = 123;
+
+let juagadorConPuntajeMasAlto = 'Fernando';
+
+class JuegoAnio {
+     
+}
