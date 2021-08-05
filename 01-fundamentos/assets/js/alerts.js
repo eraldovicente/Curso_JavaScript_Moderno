c3756ca6142db483189f1a575a419c27d8216b23
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-04 23:06:18 -0300

    Prompt, confirm y alert

--- 01-fundamentos/assets/js/alerts.js
+++ 01-fundamentos/assets/js/alerts.js
@@ -1,3 +1,12 @@
 
-// console.log( miNombre );
+// alert('Hola Mundo');
+
+// let nombre = prompt('Cuál es tu nombre?');
+// console.log( nombre );
+// console.log( '****' + nombre + '****' ); // ''
+
+// const seleccion = confirm('Está seguro de borrar esto?');
+// console.log( seleccion );
+
+console.log(global);
 
