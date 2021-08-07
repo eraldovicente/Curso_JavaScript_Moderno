27bcc49898afec8a021d92aa5c2994e75fb6fed4
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-07 18:17:39 -0300

    Optimizaciones - Tercera parte

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -1,4 +1,4 @@
-(() => {
+const miModulo = (() => {
      'use strict'
     
      let deck         = [];
@@ -18,9 +18,17 @@
      // Esta función inicializa el juego
      const inicializarJuego = ( numJugadores = 2 ) => {
           deck = crearDeck();
+
+          puntosJugadores = [];
           for( let i = 0; i < numJugadores; i++ ) {
                puntosJugadores.push(0);
           }
+
+          puntosHTML.forEach( elem => elem.innerText = 0 );
+          divCartasJugadores.forEach( elem => elem.innerHTML = '' );
+
+          btnPedir.disabled   = false;
+          btnDetener.disabled = false;
      }
 
      // Esta función crea un nuevo deck
@@ -73,6 +81,24 @@
           divCartasJugadores[turno].append( imgCarta );
      }
 
+     const determinarGanador = () => {
+
+          const [ puntosMinimos, puntosComputadora ] = puntosJugadores;
+
+          setTimeout(() => {          
+               if ( puntosComputadora === puntosMinimos ) {
+                    alert('Nadie gana :(');
+               } else if ( puntosMinimos > 21 ) {
+                    alert('Computadora gana');
+               } else if ( puntosComputadora > 21 ) {
+                    alert('Jugador Gana');
+               } else {
+                    alert('Computadora Gana');
+               }
+          }, 1000);
+
+     }
+
      // Turno de la computadora
      const turnoComputadora = ( puntosMinimos ) => {
 
@@ -89,17 +115,7 @@
 
           } while ( (puntosComputadora < puntosMinimos) && (puntosMinimos <= 21) );
 
-          setTimeout(() => {          
-               if ( puntosComputadora === puntosMinimos ) {
-                    alert('Nadie gana :(');
-               } else if ( puntosMinimos > 21 ) {
-                    alert('Computadora gana');
-               } else if ( puntosComputadora > 21 ) {
-                    alert('Jugador Gana');
-               } else {
-                    alert('Computadora Gana');
-               }
-          }, 1000);
+          determinarGanador();
      }
 
 
@@ -112,13 +128,11 @@
           crearCarta( carta, 0 );
 
           if ( puntosJugador > 21 ) {
-               console.warn('Lo siento mucho, perdiste');
                btnPedir.disabled = true;
                btnDetener.disabled = true;
                turnoComputadora( puntosJugador );
 
           } else if ( puntosJugador === 21 ) {
-               console.warn('21, genial!');
                btnPedir.disabled = true;
                btnDetener.disabled = true;
                turnoComputadora( puntosJugador );
@@ -132,29 +146,17 @@
           btnPedir.disabled = true;
           btnDetener.disabled = true;
 
-          turnoComputadora( puntosJugador );
+          turnoComputadora( puntosJugadores[0] );
      });
 
      btnNuevo.addEventListener('click', () => {
-
-          console.clear();
           inicializarJuego();
-
-          // deck = [];
-          // deck = crearDeck();
-
-          // puntosJugador     = 0;
-          // puntosComputadora = 0;
-
-          // puntosHTML[0].innerText = 0;
-          // puntosHTML[1].innerText = 0;
-
-          // divCartasComputadora.innerHTML = '';
-          // divCartasJugador.innerHTML     = '';
-
-          // btnPedir.disabled   = false;
-          // btnDetener.disabled = false;
      });
+
+
+     return {
+          nuevoJuego: inicializarJuego
+     };
 
 
 })();
