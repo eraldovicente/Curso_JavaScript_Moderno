9009097e05b37d8962502d214f7ddb516f0ded7f
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-06 23:05:28 -0300

    Valor de cada carta

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -48,7 +48,28 @@ const pedirCarta = () => {
      return carta;
 }
 
-pedirCarta();
+// pedirCarta();
+const valorCarta = ( carta ) => {
 
+     const valor = carta.substring(0, carta.length - 1);
+     return ( isNaN( valor ) ) ?
+               ( valor === 'A' ) ? 11 : 10
+               : valor * 1;
+
+     // let puntos = 0;
+
+     // if ( isNaN( valor ) ) {
+
+     //      puntos = ( valor === 'A' ) ? 11 : 10;
+
+     // } else {
+     //      puntos = valor * 1;
+     // }
+
+     // console.log(puntos);
+}
+
+const valor = valorCarta( pedirCarta() );
+console.log({ valor });
 
 
