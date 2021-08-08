25c1a88e81bbb38bf9c547382b97cb090fe386f0
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-07 21:42:04 -0300

    Problematica y necesidad de clases

---
+++ 01-fundamentos/assets/js/classes/problema.js
@@ -0,0 +1,33 @@
+
+const eraldo = {
+     nombre: 'Eraldo',
+     edad: 30,
+     imprimir() {
+          console.log(`Nombre: ${ this.nombre } - edad: ${ this.edad }`);
+     }
+}
+
+const fabiana = {
+     nombre: 'Fabiana',
+     edad: 40,
+     imprimir() {
+          console.log(`Nombre: ${ this.nombre } - edad: ${ this.edad }`);
+     }
+}
+
+// eraldo.imprimir();
+
+function Persona(nombre, edad) {
+     console.log('Se ejecutó esta línea');
+
+     this.nombre = nombre;
+     this.edad   = edad;
+     this.imprimir = function() {
+          console.log(`Nombre: ${ this.nombre } - edad: ${ this.edad }`);
+     }
+}
+
+
+const maria = new Persona('Gabriel', 6);
+console.log( maria );
+maria.imprimir();
