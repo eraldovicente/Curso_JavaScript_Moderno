b21bd4df5150613657423b0a757d3e8d2b419d26
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-07 22:20:08 -0300

    Metodos en las clases

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -6,12 +6,20 @@ class Persona {
      frase  = '';
 
      constructor( nombre = 'Sin nombre', codigo = 'Sin código', frase = 'Sin frase' ) {
-
           // if ( !nombre ) throw Error('Necesitamos el nombre')
 
           this.nombre = nombre;
           this.codigo = codigo;
           this.frase  = frase;
+     }
+
+     quienSoy() {
+          console.log(`Soy ${ this.nombre } y mi identidad es ${ this.codigo }`);
+     }
+
+     miFrase() {
+          this.quienSoy();
+          console.log(`${ this.codigo } dice: ${ this.frase }`);
      }
 }
 
@@ -20,3 +28,9 @@ const spiderman = new Persona( 'Peter Parker', 'Spider', 'Soy tu amigable vecino
 const ironman = new Persona( 'Tony Stark', 'Ironman', 'Yo soy Ironman' );
 console.log( spiderman );
 console.log( ironman );
+
+// spiderman.quienSoy(); 
+// ironman.quienSoy(); 
+
+spiderman.miFrase(); 
+ironman.miFrase(); 
