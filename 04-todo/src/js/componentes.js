e8ef553e028e9196f1fd3d56c1f154ad032879e2
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-10 20:33:36 -0300

    Construir las tareas en el HTML

--- 04-todo/src/js/componentes.js
+++ 04-todo/src/js/componentes.js
@@ -1,19 +1,24 @@
-import '../css/componentes.css';
-import webpacklogo from  '../assets/img/webpack-logo.png';
 
-export const saludar = ( nombre ) => {
+// Referencias en el HTML
+const divTodoList = document.querySelector('.todo-list');
 
-     console.log('Creando etiqueta h1');
+export const crearTodoHtml = ( todo ) => {
 
-     const h1 = document.createElement('h1');
-     h1.innerText = `Hola, ${ nombre }!!!`;
+     const htmlTodo = `
+          <li class="${ (todo.completado) ? 'completed' : '' }" data-id="${ todo.id }">
+               <div class="view">
+                    <input class="toggle" type="checkbox" ${ (todo.completado) ? 'checked' : '' }>
+                    <label>${ todo.tarea }</label>
+                    <button class="destroy"></button>
+               </div>
+               <input class="edit" value="Create a TodoMVC template">
+          </li>`;
 
-     document.body.append( h1 );
+          const div = document.createElement('div');
+          div.innerHTML = htmlTodo;
 
+          divTodoList.append( div.firstElementChild );
 
-     // Img
-     // console.log(webpacklogo);
-     // const img = document.createElement('img');
-     // img.src = webpacklogo;
-     // document.body.append( img );
+          return div.firstElementChild;
+          
 }
