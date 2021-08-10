daf5692d37c7f044594e00056507a5d6b5782ed7
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-10 18:31:51 -0300

    TodoList Class

--- 04-todo/src/index.js
+++ 04-todo/src/index.js
@@ -1,7 +1,13 @@
 import './styles.css';
 
-import { Todo } from './classes/todo.class';
+import { Todo, TodoList } from './classes';
+
+const todoList = new TodoList();
 
 const tarea = new Todo('Aprender JavaScript');
+const tarea2 = new Todo('Ir jantar');
 
-console.log(tarea);
+todoList.nuevoTodo( tarea );
+todoList.nuevoTodo( tarea2 );
+
+console.log( todoList ); 
