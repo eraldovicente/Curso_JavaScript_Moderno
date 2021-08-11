8d8664140741552885562254011aff0f2c40fd68
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-11 13:19:27 -0300

    Guardando y recuperando Todos

--- 04-todo/src/classes/todo-list.class.js
+++ 04-todo/src/classes/todo-list.class.js
@@ -3,17 +3,20 @@ export class TodoList {
 
      constructor() {
 
-          this.todos = [];
+          // this.todos = [];
+          this.cargarLocalStorage();
 
      }
 
      nuevoTodo( todo ) {
           this.todos.push( todo );
+          this.guardarLocalStorage();
      }
 
      eliminarTodo( id ) {
 
           this.todos = this.todos.filter( todo => todo.id != id );
+          this.guardarLocalStorage();
 
      }
 
@@ -26,6 +29,8 @@ export class TodoList {
                if( todo.id == id ) {
 
                     todo.completado = !todo.completado;
+                    this.guardarLocalStorage();
+                    break;
                }
           }
      }
@@ -33,14 +38,20 @@ export class TodoList {
      eliminarCompletados() {
           
           this.todos = this.todos.filter( todo => !todo.completado );
+          this.guardarLocalStorage();
 
      }
 
      guardarLocalStorage() {
 
+          localStorage.setItem('todo', JSON.stringify( this.todos ) );
      }
 
      cargarLocalStorage() {
+
+          this.todos = ( localStorage.getItem('todo') ) 
+                         ? JSON.parse( localStorage.getItem('todo') )
+                         : [];     
 
      }
      
--- 04-todo/src/index.js
+++ 04-todo/src/index.js
@@ -5,20 +5,4 @@ import { crearTodoHtml } from './js/componentes';
 
 export const todoList = new TodoList();
 
-// const tarea = new Todo('Aprender JavaScript');
-// todoList.nuevoTodo( tarea );
-
-// tarea.completado = true;
-
-// console.log( todoList ); 
-
-// crearTodoHtml( tarea );
-
-// localStorage.setItem('mi-key', 'ABC123');
-// sessionStorage.setItem('mi-key', 'ABC1234');
-
-// setTimeout( () => {
-
-//      localStorage.removeItem('mi-key');
-
-// }, 1500);
+todoList.todos.forEach( crearTodoHtml );
