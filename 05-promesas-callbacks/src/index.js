d6ec04feecd4bcb90776907ee6086a5f043c5974
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-11 19:52:53 -0300

    Argumentos estandar de los callbacks

--- 05-promesas-callbacks/src/index.js
+++ 05-promesas-callbacks/src/index.js
@@ -3,9 +3,12 @@ import './styles.css';
 
 const heroeId = 'capi';
 
-buscarHeroe( heroeId, ( heroe ) => {
-     console.log( heroe );
+buscarHeroe( heroeId, ( err, heroe ) => {
+
+     if ( err ) {
+          console.error( err );
+     } else {
+          console.info( heroe );
+     }
+
 });
-
-console.log('Fin de programa');
-
