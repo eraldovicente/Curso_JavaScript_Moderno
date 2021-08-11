264bd335a1f998675376873616aaec453b0844be
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-11 00:49:22 -0300

    LocalStorage y SessionStorage

--- 04-todo/src/classes/todo-list.class.js
+++ 04-todo/src/classes/todo-list.class.js
@@ -35,4 +35,13 @@ export class TodoList {
           this.todos = this.todos.filter( todo => !todo.completado );
 
      }
+
+     guardarLocalStorage() {
+
+     }
+
+     cargarLocalStorage() {
+
+     }
+     
 }
