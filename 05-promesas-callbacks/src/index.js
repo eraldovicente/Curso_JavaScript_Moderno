df34ea0caadb4faf31b85eb1556eeea9d19704ba
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-11 20:50:53 -0300

    Promesas

--- 05-promesas-callbacks/src/index.js
+++ 05-promesas-callbacks/src/index.js
@@ -1,28 +1,41 @@
-import { buscarHeroe } from './js/callbacks';
+import { buscarHeroe as buscarHeroeCallback } from './js/callbacks';
+import { buscarHeroe } from './js/promesas';
+
 import './styles.css';
 
 const heroeId1 = 'capi';
 const heroeId2 = 'iron';
 const heroeId3 = 'spider';
 
-buscarHeroe( heroeId1, ( err, heroe1 ) => {
 
-     if ( err ) { return console.error( err ); }
+buscarHeroe( heroeId1 ).then( heroe => {
 
-     buscarHeroe( heroeId2, ( err, heroe2 ) => {
-
-          if ( err ) { return console.error( err ); }
-
-          buscarHeroe( heroeId3, ( err, heroe3 ) => {
-
-               if ( err ) { return console.error( err ); }
-     
-               console.log(`Enviando a ${ heroe1.nombre }, ${ heroe2.nombre } y ${ heroe3.nombre } a la misión`);
-     
-          })
-
-     });
+     console.log(`Enviando a ${ heroe.nombre } a la misión`);
 
 });
+
+
+// buscarHeroe( heroeId1, ( err, heroe1 ) => {
+
+//      if ( err ) { return console.error( err ); }
+
+//      buscarHeroe( heroeId2, ( err, heroe2 ) => {
+
+//           if ( err ) { return console.error( err ); }
+
+//           buscarHeroe( heroeId3, ( err, heroe3 ) => {
+
+//                if ( err ) { return console.error( err ); }
+     
+//                console.log(`Enviando a ${ heroe1.nombre }, ${ heroe2.nombre } y ${ heroe3.nombre } a la misión`);
+     
+//           })
+
+//      });
+
+// });
+
+
+
 
 console.log('Fin de programa');
