8e2cb8ccc80294a11cfab81e7ae024b9c9962678
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-11 17:31:31 -0300

    Inicio de proyecto - Callbacks y Promesas

---
+++ 05-promesas-callbacks/webpack.prod.js
@@ -0,0 +1,91 @@
+const HtmlWebPack    = require('html-webpack-plugin');
+const MiniCssExtract = require("mini-css-extract-plugin"); 
+// const CopyPlugin     = require("copy-webpack-plugin");
+
+const CssMinimizer   = require('css-minimizer-webpack-plugin');
+const Terser         = require('terser-webpack-plugin');
+ 
+module.exports = {
+ 
+    mode: 'development',
+    output: {
+         clean: true,
+         filename: 'main.[contenthash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    sources: false,
+                    minimize: true
+                },
+            },
+            {
+               test: /\.css$/i,
+               exclude: /styles.css$/i,
+               use: ['style-loader', 'css-loader']
+            },
+            {
+                 test: /styles.css$/i,
+                 use: [ MiniCssExtract.loader, 'css-loader' ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '/assets/img/[name].[fullhash].[ext]',
+                            esModule: false
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizer(),
+            new Terser({
+                extractComments: false
+            })
+        ]
+    },
+
+    plugins: [
+        new HtmlWebPack({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtract({
+             filename: '[name].[fullhash].css',
+             ignoreOrder: false
+        }),
+        // new CopyPlugin({
+        //     patterns: [                
+        //         {
+        //             from: "src/assets/",
+        //             to: "assets/"
+        //         },
+        //     ]
+        // })
+    ]
+ 
+    
+}
+ 
+ 
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Curso_JavaScript_Moderno
 JavaScript Moderno: Guía para dominar el lenguaje - Fernando Herrera
 
-Link do projeto da atividade do módulo 4:
+Link do projeto da atividade da seção 10:
 
 https://eraldovicente.github.io/Curso_JavaScript_Moderno/
