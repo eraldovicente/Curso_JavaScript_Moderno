69ab21c27b5ba263a040d285cc0143a817984b1f
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-12 17:29:16 -0300

    Manejo de errores en el await

--- 05-promesas-callbacks/src/index.js
+++ 05-promesas-callbacks/src/index.js
@@ -1,13 +1,13 @@
-import { obtenerHeroesArr } from './js/await';
+import { obtenerHeroesArr, obtenerHeroeAwait } from './js/await';
 
 // const heroes = obtenerHeroesArr();
 console.time('await');
 
-obtenerHeroesArr().then( heroes => {
-     console.table(heroes);
-
-     console.timeEnd('await');
-});
+obtenerHeroeAwait('capi2')
+     .then( heroe => {
+          console.log( heroe ); 
+          console.timeEnd('await');
+     }).catch( console.warn );
 
 // console.table( heroes );
 // console.log( heroes );
--- 05-promesas-callbacks/src/js/await.js
+++ 05-promesas-callbacks/src/js/await.js
@@ -1,4 +1,4 @@
-import { buscarHeroe } from "./promesas";
+import { buscarHeroeAsync, buscarHeroe } from './promesas';
 
 
 const heroesIds = ['capi','iron','spider'];
@@ -7,3 +7,22 @@ const heroesIds = ['capi','iron','spider'];
 export const obtenerHeroesArr = async() => {
      return await Promise.all( heroesIds.map( buscarHeroe ) );
 };
+
+export const obtenerHeroeAwait = async( id ) => {
+
+     try {
+
+          const heroe = await buscarHeroeAsync( id );
+          return heroe;
+          
+     } catch ( err ) {
+          console.log( 'CATCH!!!' );
+          // throw err;
+          return {
+               nombre: 'Sin nombre',
+               poder: 'Sin poder'
+          };
+     }
+
+
+}
