e832a61da7e3fffd98965dbcdd70695c13b8d4c1
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-11 22:26:32 -0300

    Promise.race

--- 05-promesas-callbacks/src/index.js
+++ 05-promesas-callbacks/src/index.js
@@ -1,2 +1,11 @@
+import { promesaLenta, promesaMedia, promesaRapida } from './js/promesas';
 
-import './styles.css';
+
+// promesaLenta.then( console.log );
+// promesaMedia.then( console.log );
+// promesaRapida.then( console.log );
+
+
+Promise.race([ promesaLenta, promesaMedia, promesaRapida ])
+          .then( console.log )
+          .catch( console.warn );
