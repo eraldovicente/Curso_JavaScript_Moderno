b6759863b92b4e97e4690e1817425080f2affdb0
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-13 12:12:37 -0300

    Tarea - Crear una tabla con los usuarios

--- 06-http/src/index.js
+++ 06-http/src/index.js
@@ -1,7 +1,7 @@
-import { obtenerUsuarios } from './js/http-provider';
-// import { init } from './js/chistes-page';
+// import { obtenerUsuarios } from './js/http-provider';
+import { init } from './js/usuarios-page';
 
 
-obtenerUsuarios().then( console.log );
+// obtenerUsuarios().then( console.log );
 
-// init();
+init();
