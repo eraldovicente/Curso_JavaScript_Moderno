3190926e7151bfa83d7f3c382dfa871d68fa559d
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-12 21:49:55 -0300

    Centralizar las peticiones HTTP

--- 06-http/src/index.js
+++ 06-http/src/index.js
@@ -1,17 +1,5 @@
+const { obtenerChiste } = require("./js/http-provider");
 
-const jokeUrl = 'https://api.chucknorris.io/jokes/random';
 
-// fetch( jokeUrl ).then( resp => {
 
-//      resp.json().then( ({ id, value }) => {
-//           console.log( id );
-//           console.log( value );
-//      });
-
-// });
-
-fetch( jokeUrl )
-     .then( resp => resp.json() )
-     .then( ({ id, value }) => {
-          console.log(id, value);
-     });
+obtenerChiste().then( console.log );
