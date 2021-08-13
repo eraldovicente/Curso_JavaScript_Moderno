d633d6d86200ef14a7cf2e82ea67ee543e2237aa
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-13 12:29:22 -0300

    CRUD - Leer usuarios

--- 06-http/src/index.js
+++ 06-http/src/index.js
@@ -1,7 +1,10 @@
 // import { obtenerUsuarios } from './js/http-provider';
-import { init } from './js/usuarios-page';
-
-
+// import { init } from './js/usuarios-page';
 // obtenerUsuarios().then( console.log );
+// init();
 
-init();
+import * as CRUD from './js/crud-provider';
+
+CRUD.getUsuario( 1 ).then( console.log );
+
+
