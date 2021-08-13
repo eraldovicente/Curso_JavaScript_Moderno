f2cdc3d8824cc0923a1efafd08c11bf8eacd5a79
Eraldo Vicente <eraldovicente2@gmail.com>
2021-08-13 16:08:41 -0300

    Subir imagen a Cloudinary

--- 06-http/src/js/archivos-page.js
+++ 06-http/src/js/archivos-page.js
@@ -1,3 +1,4 @@
+import { subirImagen } from './http-provider';
 
 const body = document.body;
 let inputFile, imgFoto;
@@ -31,7 +32,9 @@ const eventos = () => {
      inputFile.addEventListener('change', (event) => {
 
           const file = event.target.files[0];
-          console.log(file);
+          // console.log(file);
+
+          subirImagen( file ).then( url => imgFoto.src = url );
 
      });
 }
